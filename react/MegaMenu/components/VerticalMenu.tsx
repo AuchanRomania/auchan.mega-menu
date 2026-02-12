d5a14430104489eb3c0c9357e8b3d86d1cf5ebcb
Andreea Musat <amusat@partner.auchan.ro>
2026-02-12 12:13:20 +0200

    feat: mobile category menu

--- react/MegaMenu/components/VerticalMenu.tsx
+++ react/MegaMenu/components/VerticalMenu.tsx
@@ -1,17 +1,18 @@
 import classNames from 'classnames'
 import { observer } from 'mobx-react-lite'
 import type { FC } from 'react'
-import React, { useMemo } from 'react'
+import React, { useState, useMemo, useCallback } from 'react'
 import type { InjectedIntlProps } from 'react-intl'
 import { injectIntl } from 'react-intl'
 import Skeleton from 'react-loading-skeleton'
 import { useCssHandles } from 'vtex.css-handles'
 import { formatIOMessage } from 'vtex.native-types'
+import { Link } from 'vtex.render-runtime'
+import { Collapsible } from 'vtex.styleguide'
 
+import type { MenuItem } from '../../shared'
 import { megaMenuState } from '../State'
-import type { ItemProps } from './Item'
 import Item from './Item'
-import Submenu from './Submenu'
 
 const CSS_HANDLES = [
   'menuContainerVertical',
@@ -20,76 +21,257 @@ const CSS_HANDLES = [
   'menuItemVertical',
   'submenuContainerVertical',
   'departmentsTitle',
+  'accordionCategoriesContainer',
+  'submenuItemVertical',
+  'submenuItem',
+  'seeAllLinkContainer',
+  'seeAllLink',
+  'collapsibleContent',
+  'collapsibleHeaderText',
+  'submenuVerticalNameContainer',
+  'menuItemIcon',
 ] as const
 
 const VerticalMenu: FC<VerticalMenuProps> = observer(({ intl }) => {
   const { handles } = useCssHandles(CSS_HANDLES)
-  const { departments, departmentActive, config, setDepartmentActive } =
-    megaMenuState
-
-  const departmentActiveHasCategories = !!departmentActive?.menu?.length
-
+  const { departments, config } = megaMenuState
   const { title } = config
+
+  // Track which department is expanded (null = none)
+  const [expandedDeptId, setExpandedDeptId] = useState<string | null>(null)
+  // Track which categories are expanded within the active department
+  const [expandedCategories, setExpandedCategories] = useState<
+    Record<string, boolean>
+  >({})
+
+  const toggleDepartment = useCallback(
+    (deptId: string) => {
+      if (expandedDeptId === deptId) {
+        setExpandedDeptId(null)
+        setExpandedCategories({})
+      } else {
+        setExpandedDeptId(deptId)
+        setExpandedCategories({})
+      }
+    },
+    [expandedDeptId]
+  )
+
+  const toggleCategory = useCallback(
+    (catId: string, isOpen: boolean) => {
+      setExpandedCategories((prev) => ({
+        ...prev,
+        [catId]: isOpen,
+      }))
+    },
+    []
+  )
 
   const departmentItems = useMemo(
     () =>
-      departments.map((d, i) => {
-        const hasCategories = !!d.menu?.length
+      departments
+        .filter((d) => d.display)
+        .map((d, i) => {
+          const hasCategories = !!d.menu?.length
+          const isExpanded = expandedDeptId === d.id
 
-        const openDepartment = () => {
-          setDepartmentActive(d)
-        }
+          return (
+            <li
+              className={classNames(handles.menuItemVertical)}
+              key={d.id}
+            >
+              {hasCategories ? (
+                <>
+                  <Item
+                    id={d.id}
+                    iconId={d.icon}
+                    accordion
+                    tabIndex={i}
+                    onClick={() => toggleDepartment(d.id)}
+                    style={d.styles}
+                    enableStyle={d.enableSty}
+                    uploadedIcon={d.uploadedIcon}
+                    isCollection={d.isCollection}
+                  >
+                    {d.name}
+                  </Item>
 
-        const itemProps: ItemProps = {
-          id: d.id,
-          iconId: d.icon,
-          accordion: hasCategories,
-          tabIndex: i,
-          onClick: openDepartment,
-          style: d.styles,
-          enableStyle: d.enableSty,
-          uploadedIcon: d.uploadedIcon,
-          isCollection: d.isCollection,
-          ...(!hasCategories && { to: d.slug }),
-        }
+                  {isExpanded && (
+                    <div
+                      className={classNames(
+                        handles.accordionCategoriesContainer,
+                        'w-100'
+                      )}
+                    >
+                      {d.menu
+                        ?.filter((cat: MenuItem) => cat.display)
+                        .map((category: MenuItem) => {
+                          const hasSubcategories = !!category.menu?.length
+                          const isCatOpen =
+                            expandedCategories[category.id] || false
 
-        return (
-          <li
-            className={classNames(
-              handles.menuItemVertical,
-              'bb b--light-gray',
-              {
-                bt: i === 0,
-              }
-            )}
-            key={d.id}
-          >
-            <Item className={classNames('pv5 mh5')} {...itemProps}>
-              {d.name}
-            </Item>
-          </li>
-        )
-      }),
+                          if (hasSubcategories) {
+                            return (
+                              <div
+                                key={category.id}
+                                className={classNames(
+                                  handles.submenuItemVertical
+                                )}
+                              >
+                                <Collapsible
+                                  header={
+                                    <div
+                                      className={handles.submenuVerticalNameContainer}
+                                    >
+                                      {category.uploadedIcon && (
+                                        <img
+                                          className={handles.menuItemIcon}
+                                          src={category.uploadedIcon}
+                                          alt=""
+                                        />
+                                      )}
+                                      <p
+                                        className={classNames(
+                                          handles.collapsibleHeaderText,
+                                          isCatOpen && 'fw7'
+                                        )}
+                                      >
+                                        {category.name}
+                                      </p>
+                                    </div>
+                                  }
+                                  align="right"
+                                  onClick={(e: any) =>
+                                    toggleCategory(
+                                      category.id,
+                                      e.target.isOpen
+                                    )
+                                  }
+                                  isOpen={isCatOpen}
+                                  caretColor="base"
+                                >
+                                  <div className={handles.collapsibleContent}>
+                                    <div
+                                      className={classNames(
+                                        handles.seeAllLinkContainer,
+                                        't-body'
+                                      )}
+                                    >
+                                      <Link
+                                        to={
+                                          category.isCollection
+                                            ? category.slug
+                                            : `${category.slug}/c`
+                                        }
+                                        className={classNames(
+                                          handles.seeAllLink,
+                                          'link underline fw7 c-on-base'
+                                        )}
+                                      >
+                                        Vezi toate produsele
+                                      </Link>
+                                    </div>
+
+                                    {category.menu
+                                      ?.filter((sub: MenuItem) => sub.display)
+                                      .map((sub: MenuItem) => (
+                                        <div
+                                          key={sub.id}
+                                          className={classNames(
+                                            handles.submenuItem,
+                                            'mt3'
+                                          )}
+                                        >
+                                          <Item
+                                            to={sub.slug}
+                                            iconId={sub.icon}
+                                            level={3}
+                                            style={sub.styles}
+                                            enableStyle={sub.enableSty}
+                                            uploadedIcon={sub.uploadedIcon}
+                                            isCollection={sub.isCollection}
+                                            optionalText={sub.optionalText}
+                                          >
+                                            {sub.name}
+                                          </Item>
+                                        </div>
+                                      ))}
+                                  </div>
+                                </Collapsible>
+                              </div>
+                            )
+                          }
+
+                          return (
+                            <div
+                              key={category.id}
+                              className={classNames(
+                                handles.submenuItemVertical
+                              )}
+                            >
+                              <Link
+                                to={
+                                  category.isCollection
+                                    ? category.slug
+                                    : `${category.slug}/c`
+                                }
+                                style={{ textDecoration: 'none' }}
+                              >
+                                <div
+                                  className={handles.submenuVerticalNameContainer}
+                                >
+                                  {category.uploadedIcon && (
+                                    <img
+                                      className={handles.menuItemIcon}
+                                      src={category.uploadedIcon}
+                                      alt=""
+                                    />
+                                  )}
+                                  <p
+                                    className={classNames(
+                                      handles.collapsibleHeaderText
+                                    )}
+                                  >
+                                    {category.name}
+                                  </p>
+                                </div>
+                              </Link>
+                            </div>
+                          )
+                        })}
+                    </div>
+                  )}
+                </>
+              ) : (
+                <Item
+                  id={d.id}
+                  to={d.slug}
+                  iconId={d.icon}
+                  tabIndex={i}
+                  style={d.styles}
+                  enableStyle={d.enableSty}
+                  uploadedIcon={d.uploadedIcon}
+                  isCollection={d.isCollection}
+                >
+                  {d.name}
+                </Item>
+              )}
+            </li>
+          )
+        }),
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [departments]
+    [departments, expandedDeptId, expandedCategories]
   )
 
   return departmentItems?.length > 0 ? (
-    <nav className={classNames(handles.menuContainerNavVertical, 'w-100')}>
-      <div
-        className={classNames(handles.departmentsContainer, {
-          dn: !!departmentActive,
-        })}
-      >
+    <nav className={handles.menuContainerNavVertical}>
+      <div className={handles.departmentsContainer}>
         <div
-          className={classNames(
-            handles.departmentsTitle,
-            'f4 fw7 c-on-base mv5 lh-copy ph5'
-          )}
+          className={handles.departmentsTitle}
         >
           {formatIOMessage({ id: title, intl })}
         </div>
-        <ul className={classNames(handles.menuContainerVertical, 'list pa0')}>
+        <ul className={classNames(handles.menuContainerVertical, 'list')}>
           {departments.length ? (
             departmentItems
           ) : (
@@ -98,20 +280,6 @@ const VerticalMenu: FC<VerticalMenuProps> = observer(({ intl }) => {
             </div>
           )}
         </ul>
-      </div>
-      <div
-        className={classNames(
-          handles.submenuContainerVertical,
-          'bg-base w-100'
-        )}
-        style={{
-          display:
-            departmentActive && departmentActiveHasCategories
-              ? 'block'
-              : 'none',
-        }}
-      >
-        <Submenu />
       </div>
     </nav>
   ) : null
